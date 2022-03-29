c910cad0f3d5030e1642cabd9e1cbaa766e27137
avandefeniks <angel.vandefeniks@gmail.com>
2022-03-28 17:47:04 -0700

    learning about variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert("This is an alert! JavaScript is running!")
+// this captures the robots name
+var playerName = window.prompt("What is your robots name?");
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+// this will do math and add 20
+console.log(10 + 10);
+
+// log robots name
+console.log("Our robot's name is " + playerName);
+
+// this creates a function called fight
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
